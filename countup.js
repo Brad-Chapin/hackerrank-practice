a109c4a9012c80ac1c620658fcf3025849db0d78
Brad Chapin <paledoor@yahoo.com>
2017-08-09 10:38:51 -0700

    initial commit

---
+++ countup.js
@@ -0,0 +1,16 @@
+function countUp(start) {
+    let str = "";
+    let val = start +1
+    let count = 0;
+    while (count < 10) {
+        if (val < start +10){
+        str += (val + " then ");
+        val++;
+        count++;
+        } else {
+            str += val;
+            count++;
+        }
+    }
+    console.log(str);
+}
